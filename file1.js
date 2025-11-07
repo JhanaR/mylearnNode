48034b9d7dd76a9f46cfa1fb05af7b0330ce2fab
jhanaR <jhanarajendran20@gmail.com>
2025-11-07 18:50:05 +0530

    today

---
+++ file1.js
@@ -0,0 +1,10 @@
+let x = 10;
+let y = 20;
+
+function f1(){
+    console.log("function1 called")
+}
+
+f1()
+
+module.exports = {x, f1};
---
+++ file2.js
@@ -0,0 +1,9 @@
+/*let obj = require("./file1.js");
+
+console.log(obj.x);
+console.log(obj.f1);*/
+
+let {x, f1} = require("./file1.js");
+
+console.log(x);
+console.log(f1);
